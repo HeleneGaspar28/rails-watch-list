40a8d14b1865f6c5741f21dea398ee11b50cf38d
Helene Gaspar <helene.gaspar@velopass.com>
2025-06-17 15:09:46 +0200

    Rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Movie.destroy_all
+List.destroy_all
+Bookmark.destroy_all
+puts "Database cleaned."
+movie_1 = Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6)
+movie_2 = Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8)
+movie_3 = Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7)
+movie_4 = Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7)
+puts "all movies created"
+
+
+list_1 = List.create!(name: "Action")
+list_2 = List.create!(name: "Drama")
+list_3 = List.create!(name: "Watch Later")
+puts "All lists created..."
+
+Bookmark.create!(movie: movie_1, list: list_1, comment: "Epic fight scenes!")
+Bookmark.create!(movie: movie_2, list: list_2, comment: "Masterpiece of storytelling.")
+Bookmark.create!(movie: movie_3, list: list_3, comment: "Need to watch this soon.")
+Bookmark.create!(movie: movie_4, list: list_1, comment: "Heist vibes!")
+puts "all bookmarks created."
+
+puts "Seed complete!"
